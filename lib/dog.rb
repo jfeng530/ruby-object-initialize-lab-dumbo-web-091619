3e830ca595b5f026055289144b39358db448fd8d
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-18 01:52:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
-class Dog 
-  def initialize=(name)
+class Dog
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-  
-  def initialize=
 end
